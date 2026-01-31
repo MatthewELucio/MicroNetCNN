52b20056efc55553621695d6ac196b1f889384f5
Annie Bai <nfk4ky@virginia.edu>
2026-01-30 21:22:06 -0500

    script to download images from micronet_annotations

--- download_images.py
+++ download_images.py
@@ -1,8 +1,15 @@
-
+import wget 
+import json 
 
 def main():
+    with open('micronet_annotations.json', 'r') as f:
+        annotations = json.load(f)
+    for uiud, data in annotations.items():
+        url = data['image_url']
+        file_name = "file" + str(data['id']) + ".jpg"
+        wget.download(url, out='micronet_images/' + file_name)
 
-  
-  
+
+
 if __name__ == '__main__':
     main()
