1370a1dd2b13f118b5b0cd4cf34a0dd8e2c00291
Annie Bai <nfk4ky@virginia.edu>
2026-01-31 18:55:17 -0500

    handled invalid urls

--- download_images.py
+++ download_images.py
@@ -1,15 +1,19 @@
 import wget 
 import json 
+import os
 
 def main():
+    if not os.path.exists('micronet_images/'):
+        os.makedirs('micronet_images/')
     with open('micronet_annotations.json', 'r') as f:
         annotations = json.load(f)
     for uiud, data in annotations.items():
         url = data['image_url']
         file_name = "file" + str(data['id']) + ".jpg"
-        wget.download(url, out='micronet_images/' + file_name)
-
-
+        try:
+            wget.download(url, out='micronet_images/' + file_name)
+        except Exception as e:
+            pass
 
 if __name__ == '__main__':
     main()
