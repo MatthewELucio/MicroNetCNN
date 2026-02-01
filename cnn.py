6ff14145bb95d5594a84af13b2ac827639770840
Matthew Lucio <bgc9yp@virginia.edu>
2026-02-01 16:07:54 -0500

    start understanding data

---
+++ cnn.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+
+def main():
+    # final training/evaluation of cnn code here
+    
+    pass
+    
+if __name__ == '__main__':
+    main()
